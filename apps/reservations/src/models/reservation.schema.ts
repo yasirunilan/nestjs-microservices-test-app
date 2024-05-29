bf6d9dde1b6dca1ff88233fffe79e572de65e637
Yasiru NIlan <nilanyasiru@gmail.com>
2024-05-29 17:06:39 +0530

    implemented modules

---
+++ apps/reservations/src/models/reservation.schema.ts
@@ -0,0 +1,25 @@
+import { AbstractDocument } from '@app/common/database/abstract.schema';
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+
+@Schema({ versionKey: false })
+export class ReservationDocument extends AbstractDocument {
+  @Prop()
+  timestamp: Date;
+
+  @Prop()
+  startDat: Date;
+
+  @Prop()
+  endDate: Date;
+
+  @Prop()
+  userId: string;
+
+  @Prop()
+  placeId: string;
+
+  @Prop()
+  invoiceId: string;
+}
+export const ReservationSchema =
+  SchemaFactory.createForClass(ReservationDocument);
